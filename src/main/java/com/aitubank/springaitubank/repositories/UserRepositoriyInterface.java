70fa4cbb6752c63e67c9a097f60e85c22349b44d
Марлан <230390@astanait.edu.kz>
2024-02-21 12:23:30 +0600

    Was created and filled getById, getBySurname and createUser.

--- src/main/java/com/aitubank/springaitubank/repositories/UserRepositoriyInterface.java
+++ src/main/java/com/aitubank/springaitubank/repositories/UserRepositoriyInterface.java
@@ -3,5 +3,8 @@ package com.aitubank.springaitubank.repositories;
 import com.aitubank.springaitubank.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface UserRepositoriyInterface extends JpaRepository<User, Integer> {
+    List<User> findBySurname(String surname);
 }
--- src/main/java/com/aitubank/springaitubank/services/UserService.java
+++ src/main/java/com/aitubank/springaitubank/services/UserService.java
@@ -1,4 +1,39 @@
 package com.aitubank.springaitubank.services;
 
-public class UserService {
+import com.aitubank.springaitubank.models.User;
+import com.aitubank.springaitubank.repositories.UserRepositoriyInterface;
+import com.aitubank.springaitubank.services.Interfaces.UserServicesInterface;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService implements UserServicesInterface {
+
+    private final UserRepositoriyInterface repo;
+
+    public UserService(UserRepositoriyInterface repo) {
+        this.repo = repo;
+    }
+
+
+    @Override
+    public List<User> getAll() {
+        return repo.findAll();
+    }
+
+    @Override
+    public User getById(int id) {
+        return repo.findById(id).orElse(null);
+    }
+
+    @Override
+    public User create(User user) {
+        return repo.save(user);
+    }
+
+    @Override
+    public List<User> getBySurname(String surname) {
+        return repo.findBySurname(surname);
+    }
 }
