29ac877491c3371a329033deea1f67cbfe8c4656
Марлан <230390@astanait.edu.kz>
2024-02-21 10:41:30 +0600

    User models finished

--- src/main/java/com/aitubank/springaitubank/models/Users.java
+++ src/main/java/com/aitubank/springaitubank/models/Users.java
@@ -1,6 +1,14 @@
 package com.aitubank.springaitubank.models;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = 'users')
 public class Users {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String name;
     private String surname;
