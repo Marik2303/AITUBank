71bfe23556ee715cfa3c3a863cda85706406afbf
nariman tleshev <230807@astanait.edu.kz>
2024-02-27 04:22:29 +0600

    dsgf

--- src/main/java/com/aitubank/springaitubank/models/usersBalance.java
+++ src/main/java/com/aitubank/springaitubank/models/usersBalance.java
@@ -1,4 +1,15 @@
 package com.aitubank.springaitubank.models;
 
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "usersBalance")
 public class usersBalance {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private double balance;
 }
