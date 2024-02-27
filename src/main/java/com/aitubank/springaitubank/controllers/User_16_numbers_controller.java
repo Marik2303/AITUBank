929210dc706ba417e73cfa914be924759798f1c4
nariman tleshev <230807@astanait.edu.kz>
2024-02-27 23:48:09 +0600

    dsgf

--- src/main/java/com/aitubank/springaitubank/controllers/User_16_numbers_controller.java
+++ src/main/java/com/aitubank/springaitubank/controllers/User_16_numbers_controller.java
@@ -1,4 +1,18 @@
 package com.aitubank.springaitubank.controllers;
 
+import com.aitubank.springaitubank.models.Users_16_numbers;
+import com.aitubank.springaitubank.services.Interfaces.UserServicesInterface;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("sixteenNumbers")
+
 public class User_16_numbers_controller {
+
+    private final UserServicesInterface service;
+
+    public User_16_numbers_controller(UserServicesInterface service){
+        this.service = service;
+    }
 }
