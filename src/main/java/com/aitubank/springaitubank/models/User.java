0cf7cd43e9bd28651cca99fffa7b88254fa272fe
adiletseilkanov <231590@astanait.edu.kz>
2024-02-29 21:22:25 +0600

    js

--- src/main/java/com/aitubank/springaitubank/models/User.java
+++ src/main/java/com/aitubank/springaitubank/models/User.java
@@ -1,4 +1,4 @@
-package com.aitubank.springaitubank.models;
+package com.aitubank.springaitubank.models; // Объявляем пакет в котором находится класс
 
 import jakarta.persistence.*;
 import lombok.Data;
@@ -8,16 +8,16 @@ import lombok.Data;
 @Table(name = "users")
 
 public class User {
-    @Id
+    @Id //Первичный ключ
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;   //Выдаем юзеру айдишку
 
     private String name;   //Выдаем юзеру имя
 
-    private String surname;
+    private String surname; //Выдаем юзеру фамилию
 
-    private boolean gender;
+    private boolean gender; //Выдаем пол юзеру
 
-    private String password;
+    private String password; //Выдаем юзеру пароль
 
 }
