05d11d82c188a454b421961ff8b496d0c71b7f31
adiletseilkanov <231590@astanait.edu.kz>
2024-03-01 23:18:23 +0600

    comments1

--- src/main/java/com/aitubank/springaitubank/controllers/UsersBalanceController.java
+++ src/main/java/com/aitubank/springaitubank/controllers/UsersBalanceController.java
@@ -4,15 +4,15 @@ import com.aitubank.springaitubank.services.Interfaces.UserServicesInterface;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RestController  //Rest Controller
 @RequestMapping("mybank")
 public class UsersBalanceController {
 
-    private final UserServicesInterface service;
+    private final UserServicesInterface service; // Приватное поле, содержащее ссылку на сервис для работы с балансом пользователей.
 
 
     public UsersBalanceController(UserServicesInterface service) {
         this.service = service;
-    }
+    } // Конструктор контроллера, принимающий в качестве аргумента сервис для работы с балансом пользователей и инициализирующий поле service.
 
 }
